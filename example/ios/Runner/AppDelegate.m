873fd00339f90584df3ad99cbcb0f914cc104d4b
Abdelhakim SAID <abdelhakim.said@commandersact.com>
2025-06-16 17:37:08 +0200

    added firebase support iOS

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+@import FirebaseCore;
+@import FirebaseAnalytics;
 
 @implementation AppDelegate
 
@@ -7,7 +9,13 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  [FIRApp configure];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (id) getFirebaseAnalyticsInstace
+{
+    return [FIRAnalytics class];
 }
 
 @end
